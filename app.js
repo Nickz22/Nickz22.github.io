b54caff941cdad952fbcf1c4348e4f9dbfe2ea5b
Nicholas Zozaya <nickzsb.nz@gmail.com>
2019-04-14 15:00:21 -0700

    finished form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('here');
+
     let setBackground2 = () => {
         document.getElementById("background").classList.remove("background-1");
         document.getElementById("background").classList.add("background-2");
@@ -17,4 +17,12 @@ console.log('here');
             document.getElementById("header-box3").classList.remove("hidden");
             clearInterval(showHeaderBox3);
         }, 1000); 
+    }
+
+    let setSfDate = () => {
+        console.log('here');
+        let inputDate = document.getElementById("input_date").value;
+        let yearMonthDay = inputDate.split("-");
+        let formattedDate = yearMonthDay[1]+'/'+yearMonthDay[2]+'/'+yearMonthDay[0];
+        document.getElementById("00N2E00000AHsDH").value = formattedDate; // set a separate field with inputted date since SF wont take Dates from a "Date" Input
     }
